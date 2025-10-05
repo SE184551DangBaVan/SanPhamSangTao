7be72f39d5c8305122fb79000851f61565e6716a
Minh Phàm Dân <phamdanminh@gmail.com>
2025-10-05 15:42:45 +0700

    MemoGame: -Changed pictures -Changed color scheme

--- src/pages/MinhPage/MinhPage.jsx
+++ src/pages/MinhPage/MinhPage.jsx
@@ -1,19 +1,19 @@
 import "./MinhPage.css"
-import BookSelector from "../../components/BookSelection/BookSelector.jsx";
+import Background from "../../assets/minhbox/img_3.png"
 import MemoGame from "../../components/MemoGame/MemoGame.jsx";
 
-import pic1 from "../../assets/minhbox/MemoGame/theFunny/1.png"
-import pic2 from "../../assets/minhbox/MemoGame/theFunny/2.png"
-import pic3 from "../../assets/minhbox/MemoGame/theFunny/3.png"
-import pic4 from "../../assets/minhbox/MemoGame/theFunny/4.png"
-import pic5 from "../../assets/minhbox/MemoGame/theFunny/5.jpg"
-import pic6 from "../../assets/minhbox/MemoGame/theFunny/6.jpg"
-import pic7 from "../../assets/minhbox/MemoGame/theFunny/7.png"
-import pic8 from "../../assets/minhbox/MemoGame/theFunny/8.jpg"
-import pic9 from "../../assets/minhbox/MemoGame/theFunny/9.png"
-import pic10 from "../../assets/minhbox/MemoGame/theFunny/10.png"
-import pic11 from "../../assets/minhbox/MemoGame/theFunny/11.jpg"
-import pic12 from "../../assets/minhbox/MemoGame/theFunny/12.jpeg"
+import pic1 from "../../assets/minhbox/MemoGame/production-grade/img.png"
+import pic2 from "../../assets/minhbox/MemoGame/production-grade/img_1.png"
+import pic3 from "../../assets/minhbox/MemoGame/production-grade/img_2.png"
+import pic4 from "../../assets/minhbox/MemoGame/production-grade/img_3.png"
+import pic5 from "../../assets/minhbox/MemoGame/production-grade/img_4.png"
+import pic6 from "../../assets/minhbox/MemoGame/production-grade/img_5.png"
+import pic7 from "../../assets/minhbox/MemoGame/production-grade/img_6.png"
+import pic8 from "../../assets/minhbox/MemoGame/production-grade/img_7.png"
+import pic9 from "../../assets/minhbox/MemoGame/production-grade/img_8.png"
+import pic10 from "../../assets/minhbox/MemoGame/production-grade/img_9.png"
+import pic11 from "../../assets/minhbox/MemoGame/production-grade/img_10.png"
+import pic12 from "../../assets/minhbox/MemoGame/production-grade/img_11.png"
 
 
 const pictures = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12];
@@ -21,7 +21,15 @@ const pictures = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, p
 const MinhPage = () =>{
     return(
         <>
-            <div className="minh-page">
+            <div className="minh-page"
+                 style={{backgroundImage: `url(${Background})`,
+                     backgroundSize: 'cover',
+                     backgroundPosition: 'center',
+                     backgroundRepeat: 'no-repeat',
+                     backgroundAttachment: 'fixed',
+                     minHeight: '100vh'
+
+            }} >
                 <MemoGame pictures={pictures}/>
             </div>
         </>
